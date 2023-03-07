80d1cf98a28ebb2a1ee08c63eb993ba04dde835e
Yanis <skyhard94@gmail.com>
2023-03-07 12:30:20 +0100

    pushing to github

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import Tilt from "react-tilt"
+import { motion } from "framer-motion"
+import { styles } from "../styles"
+import { github } from "../assets"
+import { SectionWrapper } from "../hoc"
+import { projects } from "../constants"
+import { fadeIn, textVariant } from '../utils/motion'
 
-const Works = () => {
+const ProjectCard = ({ index, name, description, tags, image, source_code_link}) => {
   return (
-    <div>Works</div>
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}> {/* on utilise la fonction fadeIn pour animer la div et index * 0.5 fait en sorte que les cartes arrivent une par une */}
+      <Tilt
+        option={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          <img 
+            src={image} 
+            alt={name}
+            className='w-full h-full object-cover rounded-2xl'
+          />
+
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img src={github} alt="github" className="w-1/2 h-1/2 object-contain" />
+            </div>
+            {/* bout de code a ajouter si on veut mettre un deuxieme rond cliquable afin d'emmener sur le site s'il est deja en ligne <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img src={github} alt="github" className="w-1/2 h-1/2 object-contain" />
+            </div> */}
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <h3>{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
   )
 }
 
-export default Works
+// comme on utilise un section wrapper qui contient une div, il faut commencer notre return par <></> pour ne pas avoir d'erreur
+const Works = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Mes projets</p>
+        <h2 className={styles.sectionHeadText}>Projets.</h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        >
+          Les projets suivants font la démonstration de mes compétences en développement web. Vous pouvez les consulter en cliquant sur le logo en haut à droite de chaque carte ci dessous, chacun étant accompagné d'une description. Ils représentent ma capacité à résoudre des problèmes complexes et à travailler avec différentes technologies.
+        </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard 
+            key={`project-${index}`} 
+            index={index}
+            {...project}
+          />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(Works, "work")
--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useState, useRef, Suspense} from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Points, PointMaterial, Preload } from '@react-three/drei'
+import * as random from "maath/random/dist/maath-random.esm"
+import { StarsCanvas } from '.'
 
-const Stars = () => {
+const Stars = (props) => {
+  const ref = useRef()
+
+
   return (
-    <div>Stars</div>
+    <group>
+      <Points ref={ref} positions={sphere} />
+    </group>
   )
 }
 
-export default Stars
+const StarCanvas = () => {
+  return (
+    <div className='w-full h-full absolute inset-0 z-[-1]'>
+      <Canvas
+        camera={{ position: [0, 0, 1] }}
+      >
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
+    </div>
+  )
+}
+
+export default StarsCanvas
